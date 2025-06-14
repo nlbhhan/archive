7cfaca212718425d792af88a90b5ac322fdeb958
nlbhhan <nguyenbaohan2210@gmail.com>
2025-06-14 13:25:27 +0700

    adjust-spacing-and-speed

--- archive-testing-letter.js
+++ archive-testing-letter.js
@@ -12,15 +12,35 @@ let moonDance;
 
 //1890
 let position1890 = 0;
-let speed1890 = 3;
-let position1890x1 = -40;
-let speed1890x1 = 5;
+let speed1890 = 1;
+let position1890x1 = -60;
+let speed1890x1 = 1;
 let position1890x2 = 50;
-let speed1890x2 = 8;
+let speed1890x2 = 1;
 
 //1901
-let position1901 = -20;
-let speed1901 = 2;
+let position1901 = 0;
+let speed1901 = 1;
+let position1901x1 = 0;
+let speed1901x1 = 8;
+let position1901x2 = 0;
+let speed1901x2 = 1;
+let thu1901 = 
+    `Tụi nó cướp lúa gạo thì thôi, còn lấy hết ruộng đất,
+cứ một dạo lại bắt nộp thêm một loại sưu cắt cổ mới.
+Em thì làm gì còn tiền mà nộp nữa hả anh,
+nên tụi nó đã bắt ông nhà em đi rồi.
+    `;
+
+//1915
+let position1915 = 0;
+let speed1915 = 10;
+let canhTac = 
+    `Tôi đây là người có dịp được ông chỉ bảo nhiều điều về canh tác ruộng đất
+và những vấn đề thường nhựt trong đoạn chúng tôi ở Bắc Kì.
+    `;
+let position1915x1 = 200;
+let speed1915x1 = 1;
 
 // let chuoiKiTu = "     .:░▒▓█";
 let chuoiKiTu1 = [
@@ -54,9 +74,6 @@ function preload() {
     scannedLetter1901 = loadImage("images/1-Letters/scanned-letter-6-1901.png");
     scannedLetter1890 = loadImage("images/post-office-1890.png");
     inconsolata = loadFont("font/Inconsolata-Light.ttf");
-    estonia = loadFont("font/Estonia-Regular.ttf");
-    syneMono = loadFont("font/SyneMono-Regular.ttf");
-    moonDance = loadFont("font/MoonDance-Regular.ttf");
 }
 
 function setup() {
@@ -64,13 +81,6 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
     angleMode(DEGREES);
     rectMode(CENTER);
-    laThu1.resize(0, windowHeight);
-    scannedLetter1915.resize(0, windowHeight/1.10);
-    scannedLetter1901.resize(0, windowHeight/1.10);
-    scannedLetter1890.resize(0, windowHeight/1.05);
-    // rectMode(CENTER);
-    // img.resize(396, 0);
-    // noLoop();
 }
 
 function draw() {
@@ -127,8 +137,8 @@ function draw() {
         position1890 = -10;
     };
     position1890 = position1890 + speed1890;
-    if (position1890x1 < -40 || position1890x1>windowHeight) {
-        position1890x1 = -40;
+    if (position1890x1 < -60 || position1890x1>windowHeight) {
+        position1890x1 = -60;
     };
     position1890x1 = position1890x1 + speed1890x1;
     if (position1890x2 < -40 || position1890x2>windowHeight) {
@@ -143,7 +153,7 @@ function draw() {
     fill('yellow');
     text("Kính gửi các ngài ở Hội đồng lợi ích kinh tế\nvà tài chính của người Pháp ở Trung Kỳ.", windowWidth/5, position1890);
     text("Mấy ngày rồi, có một tốp người gây cản trở trên biển,\ncướp bóc thuyền gạo của anh em.", windowWidth/1.5, position1890x1);
-    text("Tôi gửi thư này mong các ngài\nkhẩn cấp xem xét và xử lý,\ntrước khi tụi hải tặc này\ngây thêm khốn đốn cho các ngài.", windowWidth/2, position1890x2);
+    text("Tôi gửi thư này mong các ngài\nkhẩn cấp xem xét và xử lý,\ntrước khi tụi hải tặc này\ngây thêm khốn đốn cho các ngài.", windowWidth/1.45, position1890x2);
     textSize(20);
     textLeading(23);
     text("Tôi là lái buôn trong một công ty vận chuyển của Pháp, đóng tại xứ Nam Kì.", windowWidth/5 + 30, position1890 + 60);
@@ -153,10 +163,18 @@ function draw() {
     pop();
 
     //Thang 5 nam 1901
-    if (position1901 < -10 || position1901 > windowHeight) {
+    if (position1901 < -10 || position1901>windowHeight) {
         position1901 = -10;
     };
-    posiiton1901 = position1901 + speed1901;
+    position1901 = position1901 + speed1901;
+    if (position1901x1 < -20 || position1901x1>windowHeight) {
+        position1901x1 = -20;
+    };
+    position1901x1 = position1901x1 + speed1901x1;
+    if (position1901x2 < -20 || position1901x2 > windowHeight) {
+        position1901x2 = -20;
+    };
+    position1901x2 = position1901x2 + speed1901x2;
     push();
     textSize(32);
     textLeading(30);
@@ -164,5 +182,39 @@ function draw() {
     textFont(inconsolata);
     fill('yellow');
     text("Gửi anh thân mến!", 0, position1901);
+    text("Anh ơi, dạo này tụi Pháp lộng hành quá!", windowWidth/8, position1901x1);
+    textSize(20);
+    textLeading(23);
+    text("Tụi nó ép dân bỏ nghề gốm và dệt,\nép họ mình đi đào hầm,\nlàm mỏ mười mấy giờ đồng hồ\nNgay cả nông dân nhà mình cũng gặp đủ điều khó khăn", windowWidth/8, position1901x1 + 30);
+    text('Mong anh nhận thư này, nghe anh.', windowWidth/1.5, position1901x2);
+    textSize(16);
+    textLeading(18);
+    text(thu1901, 30, position1901 + 20);
     pop();
+
+    //Thang 7 nam 1915
+    if (position1915 < -30 || position1915 > windowHeight) {
+        position1915 = -30;
+    };
+    position1915 = position1915 + speed1915;
+    if (position1915x1 < -10 || position1915x1 > windowHeight) {
+        position1915x1 = -10;
+    };
+    position1915x1 = position1915x1 + speed1915x1;
+    push();
+    textSize(32);
+    textLeading(30);
+    noStroke();
+    textFont(inconsolata);
+    fill('yellow');
+    text("Thưa ông! Tôi đã về đến Cochinchine,\nnên muốn gửi đôi dòng đến ông và gia đình.", windowWidth/3, position1915);
+    textSize(20);
+    textLeading(23);
+    text("Bởi thế tôi cố sức nói bà con\ndùng thuốc trừ sâu bệnh cho bông lúa,\nkhi có bệnh phải tìm thấy thuốc\nlấy thuốc uống và tiêm.", windowWidth/2.5, position1915x1);
+    text(canhTac, windowWidth/8, position1915-100);
+    textSize(16);
+    textLeading(18);
+    text("Trong hai tháng giời, ông và gia đình đã\nsăn sóc và giúp đỡ anh em thợ xứ chúng tôi,\ntôi lại càng biết hơn mọi người lắm.", windowWidth/1.8, position1915-90);
+    pop();
+
 }
